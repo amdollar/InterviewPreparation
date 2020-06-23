06f221210d9447df6d2e93ec29dcd3c23f3a75f7
anurag <anuragawasthi020@gmail.com>
2020-06-23 20:44:01 +0530

    Simple Hackerrank problem solving

---
+++ BirthdayCandles.java
@@ -0,0 +1,39 @@
+package io.test.InterviewPreparation;
+/**
+ * 
+ * You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
+ * 
+ * 
+ * Complete the function birthdayCakeCandles in the editor below. It must return an integer representing the number of candles she can blow out.
+
+birthdayCakeCandles has the following parameter(s):
+
+ar: an array of integers representing candle heights
+
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ */
+
+import java.util.Arrays;
+
+public class BirthdayCandles {
+
+	public static void main(String[] args) {
+		int[] candles = new int[] { 3, 2, 1, 3 };
+		int no = birthdayCakeCandles(candles);
+		System.out.println(no);
+	}
+
+	static int birthdayCakeCandles(int[] ar) {
+		int result = 0;
+		int count = 0;
+		Arrays.sort(ar);
+		for (int i = ar.length - 1; i >= 0; i--) {
+			result = Math.max(result, ar[i]);
+			if (result == ar[i]) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
